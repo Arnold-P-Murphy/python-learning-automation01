3630f6b902d048ea645823f7399b93c225f15bf7
Arnold Murphy <user.email>
2025-04-29 14:53:44 -0600

    api_calls01

--- make_api_requests_starter_code.py
+++ make_api_requests_starter_code.py
@@ -1,14 +1,25 @@
+"""
+This module demonstrates how to: Use the requests library to make API calls.
+
+
+Author: Arnold Murphy
+Date: 2025-04-23
+"""
+
 # import necessary libraries
 import requests
 
 # define base URL
-base_url = "FILL IN"
+BASE_URL = "https://api.upcitemdb.com/prod/trial/lookup"
 
 # define parameters
 parameters = {"upc": "025000044908"}
 
 # make API request, passing in base URL and parameters
-response = requests.get(base_url, params=parameters)
+response = requests.get(BASE_URL, params=parameters, timeout=10)
 
-# print out the response URL 
+# print out the response URL
 print(response.url)
+
+
+# End-of-file (EOF)
