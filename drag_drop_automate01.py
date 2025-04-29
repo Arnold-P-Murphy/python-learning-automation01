8843fee45868233661eae41b04a1104454fdf115
Arnold Murphy <user.email>
2025-04-29 13:39:00 -0600

    update chrome driver 01

--- drag_drop_automate01.py
+++ drag_drop_automate01.py
@@ -11,10 +11,14 @@ Date: 2025-04-29
 
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # Initialize the WebDriver (e.g., Chrome)
 driver = webdriver.Chrome()
